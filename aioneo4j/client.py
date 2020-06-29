d4cce3e9f386eb5fba23fd558100474ca7ba2b30
Callum Bartlett <callum@voxelfox.co.uk>
2020-06-29 20:42:54 +0100

    Add some docstrings to stuff I think I might be using

--- aioneo4j/client.py
+++ aioneo4j/client.py
@@ -1,36 +1,35 @@
 import asyncio
 import collections
+import typing
 
 from yarl import URL
 
 from .transport import Transport
 
 
-class Client:
+class Client(object):
+    r"""A Neo4j client object, used to interface with your Neo4j database
 
-    def __init__(
-        self,
-        url='http://127.0.0.1:7474/',
-        auth=None,
-        transport=Transport,
-        request_timeout=...,
-        *, loop=None
-    ):
-        if loop is None:
-            loop = asyncio.get_event_loop()
+    Parameters
+    -----------
+    url: :class:`str`
+        The HTTP url of your database.
+    auth: Optional[:class:`tuple`]
+        A pair of (username, password) used to authenticate with your database.
+        Will be interpreted from the URL if you provided auth information there.
+    transport: Optional[:class:`Transport`]
+        The aiohttp transport method for actually performing given requests.
+    request_timeout: Optional[:class:`float`]
+        How long a request should wait to process before timing out.
+    """
 
-        self.loop = loop
-
+    def __init__(self, url:str='http://127.0.0.1:7474/', auth:typing.Tuple[str]=None, transport:Transport=Transport, request_timeout:float=..., *, loop=None):
+        self.loop = loop or asyncio.get_event_loop()
         url = URL(url)
-
         if url.user and url.password:
             auth = url.user, url.password
-
             url = url.with_user(None)
-
-            # TODO: not sure is it needed
-            url = url.with_password(None)
-
+            url = url.with_password(None)  # TODO: not sure is it needed
         self.transport = transport(
             url=url,
             auth=auth,
@@ -48,35 +47,35 @@ class Client:
 
     del get_auth, set_auth
 
-    async def data(
-        self,
-        path='db/data',
-        request_timeout=...,
-    ):
+    async def data(self, path='db/data', request_timeout=...):
+        r"""Get all the data from the database"""
+
         _, data = await self.transport.perform_request(
             'GET',
             path,
             request_timeout=request_timeout,
         )
-
         return data
 
-    async def cypher(
-        self,
-        query,
-        path='db/data/cypher',
-        request_timeout=...,
-        **params
-    ):
-        if not isinstance(query, collections.Mapping):
-            request = {'query': query}
+    async def cypher(self, query:str, path:str='db/data/cypher', request_timeout:float=..., **params):
+        r"""Run a cypher on the database
 
+        Parameters
+        -----------
+        query: :class:`str`
+            The query you want to run
+        kwargs:
+            Any of the kwargs you give the cypher will be used as input variables
+        """
+
+        # If the query is a dict, we'll assume they gave the actual POST data and go from there
+        if isinstance(query, collections.Mapping):
+            assert not params
+            request = query
+        else:
+            request = {'query': query}
             if params:
                 request['params'] = params
-        else:
-            assert not params
-
-            request = query
 
         _, data = await self.transport.perform_request(
             'POST',
@@ -84,20 +83,13 @@ class Client:
             data=request,
             request_timeout=request_timeout,
         )
-
         return data
 
-    async def transaction_commit(
-        self,
-        *statements,
-        path='db/data/transaction/commit',
-        request_timeout=...  # noqa
-    ):
-        if (
-            len(statements) == 1 and
-            isinstance(statements[0], collections.Mapping) and
-            'statements' in statements[0]
-        ):
+    async def transaction_commit(self, *statements, path='db/data/transaction/commit', request_timeout=...):
+        r"""Commit a transaction"""
+
+        # Parse out the multiple statements given
+        if len(statements) == 1 and isinstance(statements[0], collections.Mapping) and 'statements' in statements[0]:
             request = statements[0]
         else:
             request = {'statements': []}
@@ -117,7 +109,6 @@ class Client:
             data=request,
             request_timeout=request_timeout,
         )
-
         return data
 
     async def indexes(self, path='db/data/schema/index', request_timeout=...):
@@ -126,55 +117,35 @@ class Client:
             path,
             request_timeout=request_timeout,
         )
-
         return data
 
-    async def constraints(
-        self,
-        path='db/data/schema/constraint',
-        request_timeout=...,
-    ):
+    async def constraints(self, path='db/data/schema/constraint', request_timeout=...):
         _, data = await self.transport.perform_request(
             'GET',
             path,
             request_timeout=request_timeout,
         )
-
         return data
 
-    async def user_password(
-        self,
-        password,
-        username='neo4j',
-        path='user/{username}/password',
-        set_auth=False,
-        request_timeout=...,
-    ):
-        path = path.format(
-            username=username,
-        )
-
+    async def user_password(self, password, username='neo4j', path='user/{username}/password', set_auth=False, request_timeout=...):
+        path = path.format(username=username,)
         request = {'password': password}
-
         _, data = await self.transport.perform_request(
             'POST',
             path,
             data=request,
             request_timeout=request_timeout,
         )
-
         if set_auth:
             auth = username, password
-
             self.auth = auth
-
         return data
 
     async def close(self):
         await self.transport.close()
 
-    async def __aenter__(self):  # noqa
+    async def __aenter__(self):
         return self
 
-    async def __aexit__(self, *exc_info):  # noqa
+    async def __aexit__(self, *exc_info):
         await self.close()
--- aioneo4j/transport.py
+++ aioneo4j/transport.py
@@ -10,34 +10,28 @@ from . import errors  # isort:skip  # noqa
 
 
 class Transport:
+    r"""A transport object for a Neo4j client, which performs all the heavy lifting requests on the backend for the client
+
+    Parameters
+    -----------
+    url: :class:`str`
+        The base URL of the database for us to connect to
+    auth: :class:`tuple`
+        The (username, password) pair for us to authenticate with
+    request_timeout: :class:`float`
+        The timeout to be used when performing a request
+    """
 
     _auth = None
 
-    def __init__(
-        self,
-        url,
-        auth,
-        encoder=json.dumps,
-        decoder=json.loads,
-        encoder_errors=(TypeError, ValueError),
-        decoder_errors=(TypeError, ValueError),
-        request_timeout=...,
-        session=None,
-        maxsize=20,
-        use_dns_cache=False,
-        *, loop
-    ):
+    def __init__(self, url, auth, encoder=json.dumps, decoder=json.loads, encoder_errors=(TypeError, ValueError), decoder_errors=(TypeError, ValueError), request_timeout=..., session=None, maxsize=20, use_dns_cache=False, *, loop ):
         self.loop = loop
-
         self.url = url
-
         self.auth = auth
-
         self.encoder = encoder
         self.decoder = decoder
         self.encoder_errors = encoder_errors
         self.decoder_errors = decoder_errors
-
         self.request_timeout = request_timeout
 
         self.session = session
@@ -68,7 +62,6 @@ class Transport:
         self._auth = auth
 
     auth = property(get_auth, set_auth)
-
     del get_auth, set_auth
 
     @property
@@ -78,13 +71,8 @@ class Transport:
             'Accept': 'application/json; charset=UTF-8',
         }
 
-    async def _perform_request(
-        self,
-        method,
-        url,
-        params=None,
-        data=None,
-    ):
+    async def _perform_request(self, method, url, params=None, data=None):
+
         response = None
 
         try:
@@ -117,14 +105,28 @@ class Transport:
             if response is not None:
                 await response.release()
 
-    async def perform_request(
-        self,
-        method,
-        path,
-        params=None,
-        data=None,
-        request_timeout=...,
-    ):
+    async def perform_request(self, method:str, path:str, params:dict=None, data:dict=None, request_timeout:float=...):
+        r"""Perform a web request at the database
+
+        Parameters
+        -----------
+        method: :class:`str`
+            The method to be used when performing the request
+        path: :class:`str`
+            The endpoint you want to ping the request to
+        params: :class:`dict`
+            The URL parameters to use
+        data: :class:`dict`
+            The JSON data to send, if any
+        request_timeout: :class:`float`
+            The timeout you want to use when performing the request
+
+        Returns
+        --------
+        The resulting data as a (status_code, headers, data) triplet
+        """
+
+        # Encode the data
         if data is not None:
             if not isinstance(data, (str, bytes)):
                 try:
@@ -135,32 +137,36 @@ class Transport:
             if not isinstance(data, bytes):
                 data = data.encode('utf-8')
 
+        # Work out our URL
         _url = self.url / path
-
         _coro = self._perform_request(method, _url, params=params, data=data)
 
+        # Work out our timeout
         _request_timeout = request_timeout
         if request_timeout is ...:
             _request_timeout = self.request_timeout
         if _request_timeout is ...:
             _request_timeout = None
 
+        # Perform the request
         try:
             with async_timeout.timeout(_request_timeout, loop=self.loop):
                 status, headers, data = await _coro
         except asyncio.TimeoutError:
             raise errors.TimeoutError
 
+        # If we got data, let's decode that
         if data:
             try:
                 data = self.decoder(data)
             except self.decoder_errors as exc:
                 raise errors.SerializationError from exc
 
-        # TODO: nested errors?
+        # There's an error in the data? Raise that
         if isinstance(data, collections.Mapping) and data.get('errors'):
             raise errors.ClientError(data['errors'])
 
+        # Return the status code and the data given
         return status, data
 
     async def close(self):
